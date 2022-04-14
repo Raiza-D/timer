5c5e92d7fb975b54af446e48c97c5a6599b464c9
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-14 14:38:22 -0700

    Cleaned up code by removing comments. Kept commented lines at the top as a brief intro on what the code should accomplish.

--- timer1.js
+++ timer1.js
@@ -1,22 +1,19 @@
-// Make timer beep at whatever numbers the user inputs
-// E.g. node timer1.js 10 3 5 15 9
-// Computer will beep at 3, 5, 9, 10, 15 seconds
-// process.stdout.write("\x07");
+/* Challenge: Make your computer beep at whatever number intervals provided
+by user on the Terminal. E.g. node timer1.js 10 3 5 15 9
+Computer will beep at 3, 5, 9, 10, 15 seconds. 
+Use process.stdout.write("\x07"); to make computer beep. */
 
-// Store user's input into a variable so it can be sorted
-// Sort user's input (ascending order) and store it into another variable
+
 let userInput = process.argv.slice(2);
 sortedInput = userInput.sort(function(a, b) {
   return a-b;
 });
-console.log(sortedInput); // To check that elements sorted correctly
 
 const timer = function() {
   let delayInSeconds = 1000;
   for (let i = 0; i < sortedInput.length; i++) {
-    // First check if input is a number and greater than zero
     if (sortedInput[i] !== NaN && sortedInput[i] > 0) {
-      delay = delayInSeconds * sortedInput[i]; // Convert input value into milliseconds
+      delay = delayInSeconds * sortedInput[i];
       setTimeout(() => {
         process.stdout.write("\x07");
       }, delay);
@@ -24,18 +21,3 @@ const timer = function() {
   }
 };
 timer();
-
-// Make the computer beep three times, with one second apart:
-// setTimeout(() => {
-//   process.stdout.write("\x07");
-// }, 1000);
-
-// setTimeout(() => {
-//   process.stdout.write("\x07");
-// }, 2000);
-
-// setTimeout(() => {
-//   process.stdout.write("\x07");
-// }, 3000);
-
-
