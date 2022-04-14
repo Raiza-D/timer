2ea1d78e5ce7ea5b1415014a40f8c619d91f0e2c
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-14 14:34:25 -0700

    Removed if conditional that uses continue statement. This wasn't working. Conditional checks to see if input is not a NaN (not a number), meaning it is a number. Conditional also checks to make sure the input is GREATER than zero. Used the logical AND (&&) operator to avoid having two if statements. Code logic now works and takes care of the two edge cases.

--- timer1.js
+++ timer1.js
@@ -1,27 +1,27 @@
-// Make timer beat at whatever integers user inputs.
+// Make timer beep at whatever numbers the user inputs
 // E.g. node timer1.js 10 3 5 15 9
 // Computer will beep at 3, 5, 9, 10, 15 seconds
 // process.stdout.write("\x07");
 
-// Store user's input into a variable and sort it.
-// Store sorted input (ascending order) into a variable
+// Store user's input into a variable so it can be sorted
+// Sort user's input (ascending order) and store it into another variable
 let userInput = process.argv.slice(2);
 sortedInput = userInput.sort(function(a, b) {
   return a-b;
 });
-console.log(sortedInput);
+console.log(sortedInput); // To check that elements sorted correctly
 
 const timer = function() {
   let delayInSeconds = 1000;
   for (let i = 0; i < sortedInput.length; i++) {
-    // if(typeof sortedInput[i] !== "Number") {
-    //   continue;
-    // }
-    delay = delayInSeconds * sortedInput[i];
+    // First check if input is a number and greater than zero
+    if (sortedInput[i] !== NaN && sortedInput[i] > 0) {
+      delay = delayInSeconds * sortedInput[i]; // Convert input value into milliseconds
       setTimeout(() => {
         process.stdout.write("\x07");
       }, delay);
     }
+  }
 };
 timer();
 
@@ -38,5 +38,4 @@ timer();
 //   process.stdout.write("\x07");
 // }, 3000);
 
-// process.stdout.write("\x07");
 
